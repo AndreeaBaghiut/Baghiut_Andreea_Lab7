e9be2caa75cc30b95c68317527b045c6288de83a
AndreeaBaghiut <128622173+AndreeaBaghiut@users.noreply.github.com>
2023-11-27 21:14:47 +0200

    .

---
+++ Models/ShopList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Baghiut_Andreea_Lab7.Models;
+using SQLite;
+
+namespace Baghiut_Andreea_Lab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
